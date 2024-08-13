55f5cd976a3e0ff69a2ddb1eaac3c2f5f22fc39e
Luan Costa <lnmaiscedo@gmail.com>
2024-08-13 18:21:48 -0300

    correção

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import Perfil from "./components/Perfil";
-import Formulario from "./components/formulario";
+// import Formulario from "./components/formulario";
 import ReposList from "./components/ReposList";
 
 function App() {
